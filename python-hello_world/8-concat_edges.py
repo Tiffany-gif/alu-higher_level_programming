69597287fd1a591e2767780567edae9e34718306
Your Name <you@example.com>
2025-02-17 09:51:02 -0800

    Fixed issue

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an intrepreted, interactive, object-oriented programming language that combines remarkable power with very clean syntax"
+str = "Python is an intrepreted, interactive, object-oriented programming\
+        language that combines remarkable power with very clean syntax"
 str = str[39:67] + str[-22:-17] + str[:6]
 print(str)
