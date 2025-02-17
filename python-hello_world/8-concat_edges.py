62e40601421cd750fa96225d9b94f6264745505f
Your Name <you@example.com>
2025-02-17 09:31:27 -0800

    Script that prints object oriented programming with python

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Python is an intrepreted, interactive, object-oriented programming\ language that combines remarkable power with very clean syntax"
+str = str[39:67] + str [-22:-17] + str [:6]
+print(str)
