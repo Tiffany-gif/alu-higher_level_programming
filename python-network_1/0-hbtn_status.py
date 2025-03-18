5a17005bd4cfa6644b50316cb78ecb97ac2b776e
Tiffany-gif <e.umwari@alustudent.com>
2025-03-18 11:01:45 +0000

    Fisxed issue

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""__summary__
-- Write a Python script that fetches https://alu-intranet.hbtn.io/status
-- using the urllib package.
-"""
+"""Fetches  https://alu-intranet.hbtn.io/status and displays response info."""
 import urllib.request
 
-url = 'https://alu-intranet.hbtn.io/status'
-
-with urllib.request.urlopen(url) as response:
-    body = response.read()
-    print("Body response:")
-    print("\t- type:", type(body))
-    print("\t- content:", body)
-    print("\t- utf8 content:", body.decode('utf-8'))
+if __name__ == '__main__':
+    url = 'https://alu-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
